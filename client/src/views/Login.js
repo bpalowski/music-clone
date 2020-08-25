11b6f7edc0186b2e7edacf63b1bfd52c9e1033a8
Brian Palowski <brianpalowski@Brians-MacBook-Pro.local>
2020-08-25 13:52:34 -0700

    chaning href

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -49,7 +49,7 @@ class Login extends Component {
           <div className="jumboTronLogin">
 
             {/* <a href="http://localhost:5000/api/login/" id="login-anchor" >Login</a> */}
-            <a href="https://musicmocker.herokuapp/api/login" id="login-anchor" >Login</a>
+            <a href='api/login' id="login-anchor" >Login</a>
 
           </div>
 
